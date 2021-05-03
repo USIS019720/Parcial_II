d1f84a6fb41efc912bbfd5661f3554620bd3db3b
David <davidernestocernaavalos848@gmail.com>
2021-05-02 23:16:58 -0600

    se agrego la direccion url

--- app/src/main/java/com/example/parcial_2/utilidades.java
+++ app/src/main/java/com/example/parcial_2/utilidades.java
@@ -1,6 +1,6 @@
 package com.example.parcial_2;
 
 public class utilidades {
-    static String url_consulta = "";
-    static String url_mto = "";
+    static String url_consulta = "http://192.168.43.1:5984/cine/_design/cine/_view/mi-cine";
+    static String url_mto = "http://192.168.43.1:5984/cine/";
 }
